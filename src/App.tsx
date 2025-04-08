41d9694a192c1fdf065ec2d3457acdac5de3f6ec
alphatheassistant <92854101+alphatheassistant@users.noreply.github.com>
2025-04-08 23:13:31 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import { FileContextProvider } from './components/FileContextProvider';
-import { CodeBuddyChat } from './components/CodeBuddyChat';
-import { FileExplorer } from './components/FileExplorer';
-import { FileViewer } from './components/FileViewer';
 
-function App() {
-  return (
-    <FileContextProvider>
-      <div className="min-h-screen bg-gray-900 flex">
-        <div className="w-64 border-r border-gray-700">
-          <FileExplorer />
-        </div>
-        <div className="flex-1 flex flex-col">
-          <div className="h-1/2 border-b border-gray-700">
-            <FileViewer />
-          </div>
-          <div className="h-1/2">
-            <CodeBuddyChat />
-          </div>
-        </div>
-      </div>
-    </FileContextProvider>
-  );
-}
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FontProvider } from "@/contexts/FontContext";
+import { ThemeProvider } from "@/contexts/ThemeContext";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // 5 minutes
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider>
+      <FontProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </FontProvider>
+    </ThemeProvider>
+  </QueryClientProvider>
+);
 
 export default App;
